da3b81a10248d606ae6d21044883cee0a7da8e72
JoaoAlvesC <jooao.v@outlook.com>
2022-10-04 13:52:10 +0000

    create the prisma.service file and first migration with the creation of the equipment table

---
+++ prisma/migrations/20221004134854_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "equipaments" (
+    "id" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "ip_address" VARCHAR(20) NOT NULL,
+
+    CONSTRAINT "equipaments_pkey" PRIMARY KEY ("id")
+);
